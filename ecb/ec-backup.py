aa26ab65d58ed01b04ea54ac96dcb82898d3a927
Marek Bettman <mbe@enonic.com>
2015-12-17 12:43:03 +0100

    work in progress

--- ecb/ec-backup.py
+++ ecb/ec-backup.py
@@ -156,7 +156,7 @@ for dirname in all_services:
         _info("Run pre-scripts")
         for command in containers_to_backup[container_name]['pre-scripts']:
             _info(" * Execute '" + command + "' command")
-            docker_client.exec_create(container_name, command, True, True)
+            docker_client.exec_create(container=container_name, cmd=command)
 
         _info("Do backup")
         _debug("docker.exec_create(container=" + container_name + ",cmd='DO BACKUP',stdout=True,stderr=True,tty=True)")
